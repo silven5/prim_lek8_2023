750836c5fafab19225f842c33a10e1ba0855402a
silven <elvenff@gmail.com>
2023-04-25 11:59:41 +0300

    Commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { GetAllElephantsUsecase } from '../core/usecases/get-all-elephants.usecase';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage],
+  providers: [GetAllElephantsUsecase]
+})
+export class HomePageModule { }
